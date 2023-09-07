c3235a821497ff60050c1d39d41779ab704b280a
= <=>
2023-09-07 17:01:55 -0300

    segunda entrega, array & objetos

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 let manoJugador = "";
 let min = 0;
 let max = 3;
-let contadorJugador = 0;
-let contadorComputadora = 0;
-let vuelta = 1;
+let vuelta = 0;
 let manoComputadora = "";
 let resultado = "";
-let partida = "";
+let historial = [];
+let opcionesMano = [
+    {mano: "piedra", gana: "tijera", pierde: "papel"},
+    {mano: "tijera", gana: "papel", pierde: "piedra"},
+    {mano: "papel", gana: "piedra", pierde: "tijera"}
+];
+class ronda {
+    constructor (vuelta, jugador, computadora, resultado) {
+        this.vuelta = vuelta
+        this.jugador = jugador
+        this.computadora = computadora
+        this.resultado = resultado
+    }
+}
+
 function generarManoComputadora(min, max) {
     let opcionesComputadora =  ["piedra", "papel", "tijera"];
     min = Math.ceil(min);
@@ -14,69 +26,57 @@ function generarManoComputadora(min, max) {
     let i = Math.floor(Math.random() * (max - min) + min);
     return opcionesComputadora [i];
 }
-function obtenerManoJugador(){
-    let mano = "";
-    let esOpcionValida = false;
-    while (!esOpcionValida){
-        mano = window.prompt("Elige Piedra, Papel o Tijera").toLowerCase();
-        if (mano == "tijera" || mano == "papel" || mano == "piedra"){
-            esOpcionValida = true;
-        }else{
-            window.alert("La opcion no es valida");
-        }
-    }
-    return mano
-}
-function jugarMano(manoJugador, manoComputadora){
-        manoJugador = obtenerManoJugador();
+function jugarMano(i){
+    let mano = document.getElementById(i).value;
+    manoJugador = opcionesMano[mano];
         manoComputadora = generarManoComputadora(min, max);
-        if (manoJugador == "piedra" & manoComputadora == "tijera" || manoJugador == "tijera" & manoComputadora == "papel" || manoJugador == "papel" & manoComputadora == "piedra"){
-            contadorJugador = contadorJugador+1
+        if (manoJugador.gana == manoComputadora){
             resultado = "Ganaste"
-        }else if (manoComputadora == "piedra" & manoJugador == "tijera" || manoComputadora == "tijera" & manoJugador == "papel" || manoComputadora == "papel" & manoJugador == "piedra"){
-            contadorComputadora = contadorComputadora+1
+        }else if (manoJugador.pierde == manoComputadora){
             resultado = "Perdiste"
         }else {
             resultado = "Empate"
         }
-        console.log("Vuelta: " + vuelta + " " + resultado);
-        console.log(manoJugador);
-        console.log(manoComputadora);
-}
-function determinarResultado(){
-    if (contadorComputadora > contadorJugador){
-        partida = "La Computadora Gana"
-    } else if ( contadorJugador > contadorComputadora){
-        partida = "El Jugador Gana"
-    }else {
-        partida = "La Partida es un Empate"
-    }
-    console.log(partida)
-}
-function cuatroVueltas(){
-    vuelta = 1;
-    contadorComputadora = 0;
-    contadorJugador = 0;
-    console.log("Inicio de Partida");
-    for (let i = 0; i <= 3; i++){
-        jugarMano(manoJugador, manoComputadora);
-        vuelta = vuelta +1;
-    }
-    determinarResultado();
-}
-function elMejorDeTres (){
-    vuelta = 1;
-    contadorComputadora = 0;
-    contadorJugador = 0;
-    console.log("Inicio de Partida");
-    let gameOver = false;
-    while (gameOver == false){
-        jugarMano(manoJugador, manoComputadora);
-        vuelta = vuelta +1
-        if (vuelta === 4 || contadorJugador >= 2 || contadorComputadora >= 2){
-            gameOver = true;
-        }
-    }
-    determinarResultado();
+        vuelta = vuelta + 1;
+    let resultadoRonda = new ronda (vuelta, manoJugador.mano, manoComputadora, resultado);
+    historial.push(resultadoRonda);
+    console.log(historial);
 }
 
+function separarVictorias() {
+    let victorias = [];
+    let victoriasIndex = [];
+    for (const historia of historial) {
+        if (historia.resultado == "Ganaste"){
+            victorias.push(historia);
+            victoriasIndex.push(historial.indexOf(historia));
+        }
+    }
+    console.log(victorias);
+}
+function separarEmpates() {
+    let empates = [];
+    let empatesIndex =[];
+    for (const historia of historial) {
+        if (historia.resultado == "Empate"){
+            empates.push(historia);
+            empatesIndex.push(historial.indexOf(historia));
+        }
+    }
+    console.log(empates);
+}
+function separarPerdidas() {
+    let perdidas = [];
+    let pedidasIndex = [];
+    for (const historia of historial) {
+        if (historia.resultado == "Perdiste"){
+            perdidas.push(historia);
+            pedidasIndex.push(historial.indexOf(historia));
+        }
+    }
+    console.log(perdidas);
+}
+function limpiarHistorial() {
+    historial.splice(0, historial.length);
+    console.log(historial);
+}
